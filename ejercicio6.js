2ce9203a84021d7105908588d3a9b84785c590b6
ramonabramo <ramonabramo@gmail.com>
2026-01-14 10:48:36 +0100

    ejercicio6

---
+++ ejercicio6.js
@@ -0,0 +1,26 @@
+/*
+Objetivos
+- utilizar variables y constantes 
+- utilizar console.log
+
+Enunciado
+Pedir una ciudad por teclado y crear una constante llamada
+PAIS. 
+Mostrar ambas en la consola
+*/
+
+// declarar
+let ciudad = '';
+const PAIS = 'españa'; // constantes en mayusculas
+
+
+// introducir
+ciudad = prompt("Introduce la ciudad");
+
+
+// procesar
+
+// mostrar
+// mostrar informacion utilizando console
+console.log(PAIS);
+console.log(ciudad);
