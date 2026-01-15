eae847af014a0d64ff2a61c2dfe2a0bac48b3080
ramonabramo <ramonabramo@gmail.com>
2026-01-15 14:29:16 +0100

    ejercicio7

--- ejercicio8.js
+++ ejercicio8.js
@@ -1,5 +1,5 @@
 /*
-Pedir por teclado (prompt) el nombre y los apellidos de una persona y mostrarlos en pantalla utilizando alert
+Pedir por teclado (prompt) el nombre y los apellidos de una persona y mostrarlos en pantalla utilizando alert y escribirlos en la lista de la pagina
 */
 
 // declarar e inicializar
@@ -7,11 +7,20 @@ let nombre = '';
 let apellidos = '';
 
 // constantes al DOM
-
+const spanNombre = document.querySelector("#nombre");
+const spanApellidos = document.querySelector("#apellidos");
 
 // entradas
+
+nombre = prompt("Introduce tu nombre");
+apellidos = prompt("introduce tus apellidos");
 
 // procesamiento
 
 // salidas
+
+alert("Tus datos son " + nombre + " " + apellidos);
+
+spanNombre.innerHTML = nombre;
+spanApellidos.innerHTML = apellidos;
 
---
+++ ejercicio9.js
@@ -0,0 +1,19 @@
+/*
+ * Pedir por teclado el radio de una circunferencia y calculamos
+ * el area de la circunferencia de radio dado
+ *
+ * area=Math.PI * radio**2
+ * area=Math.PI * Math.pow(radio,2)
+ *
+ * Escribir en la pagina el area calculada y el radio introducido
+ */
+
+// declarar e inicializar
+
+// constantes que apunten al DOM
+
+// introducir
+
+// procesamiento
+
+// salidas
