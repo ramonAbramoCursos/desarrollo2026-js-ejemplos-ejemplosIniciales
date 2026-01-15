9aefa7c57e0f60537ef21cb8fbebd3cf921e4e0c
ramonabramo <ramonabramo@gmail.com>
2026-01-15 13:39:26 +0100

    ejercicio7

---
+++ ejercicio8.js
@@ -0,0 +1,17 @@
+/*
+Pedir por teclado (prompt) el nombre y los apellidos de una persona y mostrarlos en pantalla utilizando alert
+*/
+
+// declarar e inicializar
+let nombre = '';
+let apellidos = '';
+
+// constantes al DOM
+
+
+// entradas
+
+// procesamiento
+
+// salidas
+
