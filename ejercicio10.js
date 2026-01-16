9272d2cd5319462cef7719a7cc03511c92d18267
ramonabramo <ramonabramo@gmail.com>
2026-01-16 09:05:36 +0100

    ejercicio7

---
+++ ejercicio10.js
@@ -0,0 +1,22 @@
+/*
+Introducir un numero por teclado
+ese numero es el lado de un cuadrado
+Calcular el area y el perimetro
+area=lado*lado;
+perimetro=lado*4;
+Colocar un mensaje en la consola
+- despues de introducir el lado
+- despues de calcular el area
+- despues de calcular el perimetro
+
+Mostrar los resultados y el lado introducido en la pagina (dentro de los span)
+*/
+
+
+// declarar
+
+// introducir
+
+// procesar
+
+// mostrar
