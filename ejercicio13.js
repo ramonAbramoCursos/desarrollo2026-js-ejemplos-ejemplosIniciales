6fe8d6ed1521ed8551cc3a358e9e0abbdef1cf79
ramonabramo <ramonabramo@gmail.com>
2026-01-16 11:17:58 +0100

    ejercicio13

---
+++ ejercicio13.js
@@ -0,0 +1,15 @@
+/*
+* Pedir dos numeros por teclado
+* Calcular las operaciones suma, resta, multiplicacion y division
+
+* Mostrar :
+* En los dos li de la primera lista colocar
+    * numero1
+    * numero2
+
+* En los 4 li de la segunda lista colocar
+    * suma
+    * resta
+    * multiplicacion
+    * division
+*/
