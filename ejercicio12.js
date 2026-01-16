6f94c88750297690165b5789bdf6a0ab3c0520cd
ramonabramo <ramonabramo@gmail.com>
2026-01-16 11:12:55 +0100

    ejercicio7

---
+++ ejercicio12.js
@@ -0,0 +1,12 @@
+/**
+ * Pedir por teclado (prompt)
+ * - precio unitario
+ * - cantidad
+ * 
+ * Calcular el precio total del producto
+ * preciototal=preciounitario*cantidad
+ * 
+ * Mostrar el precio total con un 
+ * alert
+ * innerHTML
+ */
