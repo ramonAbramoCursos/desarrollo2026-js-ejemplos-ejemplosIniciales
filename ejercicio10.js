54d019a65bde9ab443fff9806ce747defed21fbc
ramonabramo <ramonabramo@gmail.com>
2026-01-19 09:00:39 +0100

    he terminado el ejercicio10 para calcular el area y el perimetro de un cuadrado. He utilizado innerHTML

--- ejercicio10.js
+++ ejercicio10.js
@@ -14,9 +14,23 @@ Mostrar los resultados y el lado introducido en la pagina (dentro de los span)
 
 
 // declarar
+let lado = 0;
+let area = 0;
+let perimetro = 0;
+
+// constantes que apunten al DOM
+const spanLado = document.querySelector("#idLado");
+const spanArea = document.querySelector("#idArea");
+const spanPerimetro = document.querySelector("#idPerimetro");
 
 // introducir
+lado = parseInt(prompt("Introduce el lado del cuadrado"));
 
 // procesar
+area = lado ** 2;
+perimetro = lado * 4;
 
 // mostrar
+spanLado.innerHTML = lado;
+spanArea.innerHTML = area;
+spanPerimetro.innerHTML = perimetro;
