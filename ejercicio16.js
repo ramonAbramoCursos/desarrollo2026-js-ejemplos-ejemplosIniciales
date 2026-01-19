2b496d978f53e656d9b728ea25ab488d00f33235
ramonabramo <ramonabramo@gmail.com>
2026-01-19 09:23:52 +0100

    2026-01-19

---
+++ ejercicio16.js
@@ -0,0 +1,19 @@
+/*
+Cambiar el contenido de los dos div 
+Colocar el contenido del primer div llamado "idUno" en el segundo div llamado "idDos" y el contenido del segundo div llamado "idDos" en el primer div llamado "idUno"
+
+*/
+
+// declarar
+let uno = '';
+let dos = '';
+
+
+
+// constantes al DOM
+
+// introducir
+
+// procesamiento
+
+// mostrar
