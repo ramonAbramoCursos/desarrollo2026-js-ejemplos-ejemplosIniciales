7e3f2c7765c327dcb06a5e6acc3336d1853ae4c8
ramonabramo <ramonabramo@gmail.com>
2026-01-19 14:08:05 +0100

    2026-01-19 13:56

---
+++ ejercicio28.js
@@ -0,0 +1,31 @@
+/**
+ * ENUNCIADO:
+ * Pedir un número de pedido por teclado (ejemplo: "45").
+ * El sistema debe formatearlo para que siempre tenga 8 dígitos, rellenando con "0" a la izquierda.
+ * Al final del código, añadir el sufijo "-ES" usando el método de relleno.
+ * Ejemplo: "45" -> "00000045-ES"
+ * Mostrarlo en el h2 con id "codigoFinal".
+**/
+/** * CONCEPTO NUEVO: .padStart() y .padEnd()
+ * cadena.padStart(longitud, "relleno") -> Rellena al principio.
+ * cadena.padEnd(longitud, "relleno") -> Rellena al final.
+ * Ejemplo:
+ * numero="45";
+ * numero.padStart(8, "0"); // Devuelve "00000045"
+ * numero.padEnd(8, "-ES"); // Devuelve "00000045-ES"
+ *
+ */
+
+// declarar
+
+
+// constantes al DOM
+
+
+// introducir
+
+
+// procesamiento
+
+
+// salidas
