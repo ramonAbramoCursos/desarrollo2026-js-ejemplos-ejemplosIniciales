5f12675b38317348487acaa53f8254f5729ba2aa
ramonabramo <ramonabramo@gmail.com>
2026-01-19 10:07:41 +0100

    2026-01-19

---
+++ ejercicio20.js
@@ -0,0 +1,29 @@
+/**
+ * Pedir por teclado el precio de un producto (base imponible).
+ * Calcular el valor del IVA (21%) y el precio total.
+ * 
+ * Utilizar el metodo toFixed(2) del objeto Number para redondear a dos decimales
+ * Este metodo devuelve un texto
+ * numero=2.3456
+ * numero.toFixed(2) = "2.35"
+ * 
+ * Mostrar los tres datos en el HTML dentro de los span que estan dentro de los li
+ */
+
+// declarar
+let base = 0.0,
+    valorIva = 0.0,
+    precioFinal = 0.0;
+
+// constantes que apunten al DOM
+const spanBase = document.querySelector("#");
+const spanIva = document.querySelector("#");
+const spanTotal = document.querySelector("#");
+
+// introducir
+
+
+// procesar
+
+
+// mostrar
---
+++ ejercicio21.js
@@ -0,0 +1,22 @@
+/**
+ * Pedir por teclado el radio de la base y la altura de un cilindro.
+ * Calcular el volumen usando la fórmula: V = π * r² * h
+ * Usar Math.PI para el número Pi.
+ * Mostrar el resultado con un alert y en el HTML.
+ */
+
+// declarar
+
+
+// constantes que apunten al DOM
+
+
+// introducir
+radio = Number(prompt("Introduce el radio del cilindro:"));
+altura = Number(prompt("Introduce la altura del cilindro:"));
+
+// procesar
+
+
+// mostrar
+nodoVolumen.innerHTML = volumen.toFixed(3) + " unidades cúbicas";
