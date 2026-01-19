e21c8f820069a58b4c9e5ed37a5575cde77c6af6
ramonabramo <ramonabramo@gmail.com>
2026-01-19 09:56:38 +0100

    2026-01-19

---
+++ ejercicio18.js
@@ -0,0 +1,21 @@
+/**
+ * Pedir al usuario la temperatura en grados Celsius.
+ * Calcular la conversión a Fahrenheit usando la fórmula: 
+ * F = (C * 1.8) + 32
+ * Mostrar ambos valores en sus respectivos span.
+ */
+
+// declarar
+
+
+// constantes que apunten al DOM
+
+
+// introducir
+
+// procesar
+gradosF = (gradosC * 1.8) + 32;
+
+// mostrar
+spanC.innerHTML = gradosC + "°C";
+spanF.innerHTML = gradosF + "°F";
