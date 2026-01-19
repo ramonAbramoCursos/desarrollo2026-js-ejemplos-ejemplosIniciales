33ab1511cb50c60ca5b1245d936fb26c7b6cdde5
ramonabramo <ramonabramo@gmail.com>
2026-01-19 09:54:53 +0100

    2026-01-19

---
+++ ejercicio17.js
@@ -0,0 +1,22 @@
+/**
+ * Pedir por teclado (prompt) el valor de los dos catetos de un triángulo rectángulo.
+ * Calcular la hipotenusa usando la fórmula: 
+ * h = raíz cuadrada(c1² + c2²)
+ *  Mostrar el resultado 
+ * - en el span con id "resultado" 
+ * - mediante un alert.
+ */
+
+// declarar
+
+
+// constantes que apunten al DOM
+
+
+// introducir
+;
+
+// procesar
+
+
+// mostrar
