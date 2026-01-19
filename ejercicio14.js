8baca534a186cf6b88ffe9929b5240dde4ba7471
ramonabramo <ramonabramo@gmail.com>
2026-01-19 09:17:23 +0100

    2026-01-19

---
+++ ejercicio14.js
@@ -0,0 +1,16 @@
+/**
+ * Quiero que me coloque js
+ * en el primer div mi nombre
+ * en el segundo div "ejemplo de clase"
+ * en el tercer div "Alpe"
+ */
+
+// necesito una constante que apunte a cada elemento de la web
+// sobre el que quereis leer o escribir
+
+// metodos para acceder a los elementos del DOM
+// document.querySelector("selector")
+// selectores de css
+// etiqueta (div,span,a)
+// class (.clase)
+// id (#id)
